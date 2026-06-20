17a272260900476aee569907eb370d16c8f4223f
SizableLlama <williamagrobb@gmail.com>
2026-06-20 23:34:00 +0100

    Refactor Parser class access modifiers and methods
    updated all indentations to 8 spaces instead of 4

--- parser.hpp
+++ parser.hpp
@@ -25,103 +25,103 @@
 
 
 class Parser final {
-private:
-    std::vector<Token> tokens;
-    int current = 0;
+        private:
+                std::vector<Token> tokens;
+                int current = 0;
 
-public: // <-- Constructor must be public so main.cpp can use it!
-    Parser(std::vector<Token> tokens) {
-        this->tokens = tokens;
-    }
-
-    
-    
-    bool isAtEnd() {
-        return current >= tokens.size() || tokens.at(current).type == END_OF_FILE;
-    }
-
-    
-    Token peek() {
-        if (isAtEnd()) return Token(END_OF_FILE, "", std::any{}, 0);
-        return tokens.at(current);
-    }
-
-    
-    Token advance() {
-        if (!isAtEnd()) current++;
-        return tokens.at(current - 1);
-    }
-
-    
-    bool match(TokenType expected) {
-        if (isAtEnd()) return false;
-        
-        
-        if (peek().type != expected) return false;
-
-        current++;
-        return true;
-    }
-
-    void foo(){
-	    std::cout<<"foo\n";
-    }
-   
-
-    // The parser filters the Tokens into either Command, or Pipeline.
-   	Pipeline parseExample() {
-		Pipeline pipeline;
-		Command currentCommand; 
-		while (!isAtEnd()) {
-		 
-		
-			if (match(WORD) || match(STRING)) {
-				currentCommand.args.push_back(tokens[current].lexeme);
-			}
-			else if(match(PIPE)) {
-				pipeline.commands.push_back(currentCommand);
-			}
-			else if (match(RIGHT)) {
-				currentCommand.redir_type = RedirType::OUTPUT;
-				//Here i expect a string, aka, a file name.
-				//And so, I must append the next token, which should happend anyway, 
-				if (match(WORD) || match(STRING)) {
-					//No idea if this is right. Also don't know what is the OS's job
-					currentCommand.redir_file = tokens[current].lexeme;
-					currentCommand.args.push_back(tokens[current].lexeme);
-				}
-				else{
-					std::cerr<<"expected file name following '>'\n";
-				}
-			}
-			else if (match(LEFT)) {
-				currentCommand.redir_type = RedirType::INPUT;
-
-			}
-			else if (match(APPEND)) {
-				currentCommand.redir_type = RedirType::APPEND;
-			}
-			else if (match(AMP)) {
-				currentCommand.background = true;
-			}
-			else if (match(SEMICOLON)) {
-				pipeline.commands.push_back(currentCommand);
-			}
-			else if (match(AND_AND)) {
-				pipeline.commands.push_back(currentCommand);
-			}
-			else if (match(OR_OR)) {
-				pipeline.commands.push_back(currentCommand);
-			}
-			else if (match(END_OF_FILE)) {
-				break;
-			}
+        public: // <-- Constructor must be public so main.cpp can use it!
+                Parser(std::vector<Token> tokens) {
+                        this->tokens = tokens;
+                }
 
 
 
-			advance(); 
-		}
+                bool isAtEnd() {
+                        return current >= tokens.size() || tokens.at(current).type == END_OF_FILE;
+                }
 
-	return pipeline;
-	}
+
+                Token peek() {
+                        if (isAtEnd()) return Token(END_OF_FILE, "", std::any{}, 0);
+                        return tokens.at(current);
+                }
+
+
+                Token advance() {
+                        if (!isAtEnd()) current++;
+                        return tokens.at(current - 1);
+                }
+
+
+                bool match(TokenType expected) {
+                        if (isAtEnd()) return false;
+
+
+                        if (peek().type != expected) return false;
+
+                        current++;
+                        return true;
+                }
+
+                void foo(){
+                        std::cout<<"foo\n";
+                }
+
+
+                // The parser filters the Tokens into either Command, or Pipeline.
+                Pipeline parseExample() {
+                        Pipeline pipeline;
+                        Command currentCommand;
+                        while (!isAtEnd()) {
+
+
+                                if (match(WORD) || match(STRING)) {
+                                        currentCommand.args.push_back(tokens[current].lexeme);
+                                }
+                                else if(match(PIPE)) {
+                                        pipeline.commands.push_back(currentCommand);
+                                }
+                                else if (match(RIGHT)) {
+                                        currentCommand.redir_type = RedirType::OUTPUT;
+                                        //Here i expect a string, aka, a file name.
+                                        //And so, I must append the next token, which should happend anyway, 
+                                        if (match(WORD) || match(STRING)) {
+                                                //No idea if this is right. Also don't know what is the OS's job
+                                                currentCommand.redir_file = tokens[current].lexeme;
+                                                currentCommand.args.push_back(tokens[current].lexeme);
+                                        }
+                                        else{
+                                                std::cerr<<"expected file name following '>'\n";
+                                        }
+                                }
+                                else if (match(LEFT)) {
+                                        currentCommand.redir_type = RedirType::INPUT;
+
+                                }
+                                else if (match(APPEND)) {
+                                        currentCommand.redir_type = RedirType::APPEND;
+                                }
+                                else if (match(AMP)) {
+                                        currentCommand.background = true;
+                                }
+                                else if (match(SEMICOLON)) {
+                                        pipeline.commands.push_back(currentCommand);
+                                }
+                                else if (match(AND_AND)) {
+                                        pipeline.commands.push_back(currentCommand);
+                                }
+                                else if (match(OR_OR)) {
+                                        pipeline.commands.push_back(currentCommand);
+                                }
+                                else if (match(END_OF_FILE)) {
+                                        break;
+                                }
+
+
+
+                                advance();
+                        }
+
+                        return pipeline;
+                }
 };
