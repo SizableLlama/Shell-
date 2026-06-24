feb45b5430dc830f71401d381cabe656fb3bb12e
SizableLlama <williamagrobb@gmail.com>
2026-06-24 02:24:03 +0100

    Refactor Executioner class and add built-in command handling

--- executioner.hpp
+++ executioner.hpp
@@ -11,8 +11,7 @@ void foo();
 // this should all be in a class executioner.
 
 class Executioner {
-public:
-
+private:
 	std::vector<char*> commandToArgv(const Command& command) {
 		std::vector<char*> argv;
 		for (const std::string& arg : command.args) {
@@ -21,39 +20,67 @@ public:
 		argv.push_back(nullptr);  // required sentinel — execvp scans until it sees this
 		return argv;
 	}
-	
-	void run(const Command& command, const Pipeline& pipeline) {
+
+	bool builtIn(const Command& command) {
+		if (command.args[0] == "cd") {
+			if (command.args.size() < 2) {
+				char* home = getenv("HOME");
+				chdir(home);
+				return true;
+			}
+			else {
+				chdir(command.args[1].c_str());
+				return true;
+			}
+		}
+		else if (command.args[0] == "exit") {
+			std::cout<<std::exit;
+			return true;
+		}
+		return false;
+	}
+
+
+
+
+
+
+
+
+
+
+/*
+TODO empty string, eg "hi  with no closing indicator results in a crash.
+TODO benchmark
+TODO figure out the wait function
+TODO stop ctrl + c
+TODO make a nice main
+TODO add functionality to && || | > < >>
+*/
+
+public:
+	Executioner() = default;
+	void run(const Pipeline& pipeline) {
 		//TODO make this elif chain a switch. Also, check for built ins before commandToArgv.
-		std::vector<char*>argv = commandToArgv(command);
+		const Command& com = pipeline.commands[0];
+		std::vector<char*> argv = commandToArgv(com);
+		if (builtIn(com)) return;
+		int z = 0;
 		size_t x = 0;
+		int pointed = 0;
 		while (x <= pipeline.commands.size()) {
 
 			if (fork() == 0) {
-
-
-				if ((std::string)argv[0] == "cd" ) {
-					int err = 0;
-					err = chdir(argv[1]);
-					if (err == 1 ) {
-						std::cerr<<"There was an error.\n";
-						break;
-					} 
-				}
-				else if ((std::string)argv[0] == "exit") {
-					//TODO need to think of a better way to do this.
-					std::cout<<std::exit;
-				}
-				else {
-					execvp(argv[0], argv.data());
-				}
-
+				execvp(argv[0], argv.data());
+				pid_t wait(int *pointed);
+				std::cout<<"Forked "<<z<<" times.\n";
+				z++;
 			}
 			else {
-				std::cerr<<"There was an error.\n";
+				pid_t wait(int *pointed);
+				std::cerr<<"There was an error forking. For the "<<z<<"time \n"<<pointed<<"\n";
+				z++;
 			}
-
-
-
 			x++;
 		}
 		
