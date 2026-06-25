74f8f687836fed4a33b8d495dabd59e7b2a31243
SizableLlama <williamagrobb@gmail.com>
2026-06-25 02:14:07 +0100

    Clean up comments and unused code in parser.hpp
    Removed commented-out code and unnecessary comments from parser.hpp.

--- parser.hpp
+++ parser.hpp
@@ -4,26 +4,9 @@
 #include "lexer.hpp"
 #include "structs.hpp"
 #include "shellToken.hpp"
-
 #include <vector>
-//no clue what i'm doing, just know i need structs and something else.
 #include <fstream>
 #include <iostream>
-
-//TODO change all the else if s to a switch
-
-//If source is a string, maybe the tokens should be a "source" ?
-
-
-
-//The parser filters the Tokens into either Command, or Pipeline.
-//it can do this by adding rules to the structs. Think about it.
-//what is the criteria for a command versus a pipeline?
-
-//It all makes sense, at long last. The parser takes tokens, and stores them in the args vector.
-//When it finds a | it saves the command and starts a new one!!!
-
-
 
 class Parser final {
         private:
@@ -69,7 +52,6 @@ class Parser final {
                 }
 
 
-                // The parser filters the Tokens into either Command, or Pipeline.
                 Pipeline parse() {
                         Pipeline pipeline;
                         Command currentCommand;
@@ -93,11 +75,7 @@ class Parser final {
                                 }
                                 else if (match(RIGHT)) {
                                         currentCommand.redir_type = RedirType::OUTPUT;
-                                        //Here i expect a string, aka, a file name.
-                                        //And so, I must append the next token, which should happend anyway, 
                                         if (match(WORD) || match(STRING)) {
-                                                //No idea if this is right. Also don't know what is the OS's job
-						//match consumes a token and so i hope this is right.
                                                 currentCommand.redir_file = tokens[current - 1].lexeme;
 						continue;
                                         }
@@ -112,7 +90,6 @@ class Parser final {
                                 }
                                 else if (match(LEFT)) {
                                         currentCommand.redir_type = RedirType::INPUT;
-					//command like : commmand < input_file
 					if (match(WORD) || match(STRING)) {
 						currentCommand.redir_file = tokens[current - 1].lexeme;
 						currentCommand.args.push_back(tokens[current - 1].lexeme);
@@ -127,7 +104,6 @@ class Parser final {
                                 }
                                 else if (match(APPEND)) {
                                         currentCommand.redir_type = RedirType::APPEND;
-					//command like : command >> output_file
 					if (match(WORD) || match(STRING)) {
 						currentCommand.redir_file = tokens[current - 1].lexeme;
 						continue;
@@ -170,15 +146,6 @@ class Parser final {
 						std::cerr<<"Expected expression following '||'\n";
 					}
                                 }
-                                //else {
-                                //        pipeline.commands.push_back(currentCommand);
-				//	currentCommand = Command();
-				//	break;
-                                //}
-				
-				
-
-
 
                                 advance();
                         }
